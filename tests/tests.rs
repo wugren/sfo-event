0e66934058f5d457de4eb1ffbbf58feaf92f0534
wugren <wugren@gmail.com>
2025-06-20 16:47:38 +0800

    modify publish info

---
+++ tests/tests.rs
@@ -0,0 +1,23 @@
+use sfo_event::sfo_event;
+
+#[async_trait::async_trait]
+#[sfo_event]
+trait TestTrait1: 'static + Sync + Send {
+    async fn test(&self) -> Result<(), ()>;
+    fn test2(&self) -> Result<(), ()>;
+}
+
+
+#[async_trait::async_trait]
+#[sfo_event]
+pub trait TestTrait2<T:'static + Send + Sync>: 'static + Sync + Send {
+    async fn test(&self, t: &T) -> Result<(), ()>;
+}
+
+#[test]
+fn test_trait() {
+    let emitter = TestTrait1Emitter::new();
+    // emitter.add_listener(TestTrait1Listener::new());
+    emitter.test2();
+}
+
